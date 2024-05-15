2f93bee6a3ebb97ca461b17c6f8727ba1af5de79
TLOUDMATLOU <tmatlou11@gmail.com>
2024-05-15 15:50:25 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,19 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEwd46xJrhgStzUE7G1gR-n2Nds3UWAIE",
+  authDomain: "ctu-buddy-4bbe1.firebaseapp.com",
+  projectId: "ctu-buddy-4bbe1",
+  storageBucket: "ctu-buddy-4bbe1.appspot.com",
+  messagingSenderId: "225436749013",
+  appId: "1:225436749013:web:819f75c33dd83d80e4e65c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
